957d473e26ef07df7284c8bb1e7f47c087e1de94
Joshua Hartmann <joshuahartmann@Air-von-Joshua.int.netways.de>
2023-07-06 14:36:26 +0200

    add resources

---
+++ Foreman_Font.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Foreman_Font" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="Foreman_Font.png" width="160" height="32"/>
+</tileset>
---
+++ Technical.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Technical" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="Technical.png" width="160" height="32"/>
+</tileset>
